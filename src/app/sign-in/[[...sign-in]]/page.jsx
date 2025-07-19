57ded227ed862e1aa9c275f35890088d5daa3547
Andy <andy.lamyt@gmail.com>
2025-07-19 01:38:35 +0100

    Edited: tsconfig.json to fix deployment bug

--- src/app/sign-in/[[...sign-in]]/page.jsx
+++ src/app/sign-in/[[...sign-in]]/page.jsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex w-full justify-center items-center mt-[70px] ">
-      <SignIn />;
+      <SignIn />
     </div>
   );
 }
